ca0b00f4c5fd26aab1229e105acbd38e9f5eff3b
conradkoh <conradkoh@gmail.com>
2023-09-24 17:33:48 +0800

    added support for tailwind css

---
+++ src/lib/components/page/PageStyles.svelte
@@ -0,0 +1,16 @@
+<div class="page">
+	<div class="content">
+		<slot />
+	</div>
+</div>
+
+<style lang="scss">
+	.page {
+		@apply min-h-screen font-serif;
+	}
+	.page {
+		.content {
+			@apply px-2 pt-2 max-w-2xl mx-auto;
+		}
+	}
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { page } from '$app/stores';
+	import { fade } from 'svelte/transition';
+	import '../app.css';
+</script>
+
+{#key $page.url.pathname}
+	<main class="min-h-screen" in:fade>
+		<slot />
+	</main>
+{/key}
