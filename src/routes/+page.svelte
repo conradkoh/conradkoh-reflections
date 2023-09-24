4187f2667a0de823d5ab013a53d45d7116f411f8
conradkoh <conradkoh@gmail.com>
2023-09-24 18:11:29 +0800

    updated reflections entry page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,20 @@
 </script>
 
 <PageStyles>
-	<h1>Reflections.</h1>
-	<p class="text-sm text-gray-500 text-right">by Conrad Koh</p>
+	<h1 class="text-5xl block text-center">Reflections.</h1>
+	<span class="block pt-3 pb-3 text-sm text-gray-500 text-center">Conrad Koh</span>
+	<p>Words are an old tradition.</p>
+	<p>
+		As with any tradition, it have a stale taste, or a musty smell. Yet perhaps like stepping into a
+		shop full of antiques, a sense of wonder remains, for the hope of knowledge existed before, but
+		now forgotten.
+	</p>
+	<p>
+		There is still much to be remember. And hopefully to one day be called to recollection by one
+		who deems it worthy.
+	</p>
+	<p>
+		This is my recitation, my reflection, my attempt to remember. I hope you find something of use
+		here.
+	</p>
 </PageStyles>
