b44ff2244b6b7c86d1bcadf76af296c3d515c40e
conradkoh <conradkoh@gmail.com>
2023-09-24 17:34:12 +0800

    added post component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,8 @@
-<h1>Reflections</h1>
+<script>
+	import PageStyles from '$lib/components/page/PageStyles.svelte';
+</script>
+
+<PageStyles>
+	<h1>Reflections.</h1>
+	<p class="text-sm text-gray-500 text-right">by Conrad Koh</p>
+</PageStyles>
