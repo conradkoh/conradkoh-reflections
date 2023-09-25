de2e0dfda7d2021a775b5902891532af4ac7ce4b
conradkoh <conradkoh@gmail.com>
2023-09-25 09:07:34 +0800

    Updated font sizes to improve readability, added page titles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,9 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<title>{$page.data.title || "Conrad's Reflections."}</title>
+</svelte:head>
 {#key $page.url.pathname}
 	<main class="min-h-screen" in:fade>
 		<slot />
